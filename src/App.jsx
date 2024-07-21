bb5a444bf57756ea3d4d574afa90f4e63b96f95b
agung harwin n <agungharwinn@gmail.com>
2024-07-21 18:03:53 +0700

    fun project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Highlights from "./components/Highlights";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+}
+
+export default App;
