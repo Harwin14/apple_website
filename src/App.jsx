747ffffbc081feaaeae567ced5415bfce4570633
agung harwin n <agungharwinn@gmail.com>
2024-07-25 11:23:20 +0700

    Add highlights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Model from "./components/Model";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
